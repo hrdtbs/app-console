cf317be364656e58cd003e7defb75eb950641b28
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-09-13 16:21:32 +0900

    feat(apps/todo-app): update

--- src/apps/todo-app/App.tsx
+++ src/apps/todo-app/App.tsx
@@ -1,21 +1,40 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 export default () => {
-    const [list, setList] = useState<string[]>([])
-    return (
-        <div>
-            <form onSubmit={e => {
-                e.preventDefault()
-                const list = Array.from(new FormData(e.currentTarget).values()) as string[]
-                setList(prev => [...prev, ...list])
-            }}>
-                <input type="text" name="app"/>
-                <button>add</button>
-            </form>
-            <div>
-                {list.map(item => {
-                    return <button key={item}>{item}</button>
-                })}
-            </div>
-        </div>
-    )
-}
+  const [list, setList] = useState<string[]>([]);
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const item = Array.from(
+            new FormData(e.currentTarget).values()
+          )[0] as string;
+          setList((prev) => {
+            if (prev.includes(item)) {
+              return prev;
+            } else {
+              return [...prev, item];
+            }
+          });
+        }}
+      >
+        <input type="text" name="app" />
+        <button>add</button>
+      </form>
+      <div>
+        {list.map((item) => {
+          return (
+            <button
+              key={item}
+              onClick={() => {
+                setList((prev) => prev.filter((i) => i !== item));
+              }}
+            >
+              {item}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
